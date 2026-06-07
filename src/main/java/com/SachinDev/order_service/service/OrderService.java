994e4d8af3a12104637a0d6117b66f3e629e020d
Sachin Hadimani <sachina0075@gmail.com>
2026-06-07 21:26:15 +0530

    update with discovery server instead of port

--- src/main/java/com/SachinDev/order_service/service/OrderService.java
+++ src/main/java/com/SachinDev/order_service/service/OrderService.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest)
     {
@@ -33,8 +33,8 @@ public class OrderService {
                 .map(OrderLineItem::getSkuCode).toList();
 
         //call inventory service and if it exists then place order
-        InventoryResponse[] inventoryResponsesArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponsesArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                                 uriBuilder->uriBuilder.queryParam("skuCode", skuCodes)
                         .build())
                 .retrieve()
